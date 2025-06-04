62d820111d4bd5d9e18ece1f91103e2b71ad88c4
KaykySG <kaykyfsoares@hotmail.com>
2025-06-03 23:46:05 -0300

    Implementando a service Subwoofer

---
+++ Back-API-MTGAudioCar/src/main/java/com/vpk/backapimtgaudiocar/service/SubwooferService.java
@@ -0,0 +1,56 @@
+package com.vpk.backapimtgaudiocar.service;
+
+import com.vpk.backapimtgaudiocar.model.Subwoofer;
+import com.vpk.backapimtgaudiocar.repository.SubwooferRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SubwooferService {
+
+    @Autowired
+    private SubwooferRepository subwooferRepository;
+
+    public List<Subwo,0,1ofer> listarTodos() {
+        return subwooferRepository.findAll();
+    }
+
+    public Optional<Subwoofer> buscarPorId(String id) {
+        return subwooferRepository.findById(id);
+    }
+
+    public Subwoofer salvar(Subwoofer subwoofer) {
+        return subwooferRepository.save(subwoofer);
+    }
+
+    public Subwoofer atualizar(String id, Subwoofer subwooferAtualizado) {
+        return subwooferRepository.findById(id).map(subwoofer -> {
+            subwoofer.setModelo(subwooferAtualizado.getModelo());
+            subwoofer.setMarca(subwooferAtualizado.getMarca());
+            subwoofer.setPotenciaRmsW(subwooferAtualizado.getPotenciaRmsW());
+            subwoofer.setPotenciaMaximaW(subwooferAtualizado.getPotenciaMaximaW());
+            subwoofer.setImpedanciaOhms(subwooferAtualizado.getImpedanciaOhms());
+            subwoofer.setTipoBobina(subwooferAtualizado.getTipoBobina());
+            subwoofer.setSensibilidadeDb(subwooferAtualizado.getSensibilidadeDb());
+            subwoofer.setFaixaFrequenciaHz(subwooferAtualizado.getFaixaFrequenciaHz());
+            subwoofer.setTipoCaixaIdeal(subwooferAtualizado.getTipoCaixaIdeal());
+            subwoofer.setVolumeCaixaLitros(subwooferAtualizado.getVolumeCaixaLitros());
+            subwoofer.setDiametroPolegadas(subwooferAtualizado.getDiametroPolegadas());
+            subwoofer.setImagemUrl(subwooferAtualizado.getImagemUrl());
+            subwoofer.setDescricao(subwooferAtualizado.getDescricao());
+            subwoofer.setCategoriaId(subwooferAtualizado.getCategoriaId());
+            return subwooferRepository.save(subwoofer);
+        }).orElseGet(() -> {
+            subwooferAtualizado.setId(id);
+            return subwooferRepository.save(subwooferAtualizado);
+        });
+    }
+
+    public void deletar(String id) {
+        subwooferRepository.deleteById(id);
+    }
+}
+
