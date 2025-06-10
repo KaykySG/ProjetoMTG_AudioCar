ee1535d377dda11c664a02afb3ebd7e048ef278b
Vinicius Fernandes da Silva <107755602+Anvicii@users.noreply.github.com>
2025-06-10 10:04:41 -0300

    Implementação do Auth.interceptor
    Agora verifica os acessos.

--- Front/src/app/app.config.ts
+++ Front/src/app/app.config.ts
@@ -1,19 +1,27 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
+import { authInterceptor } from './auth.interceptor'; // <-- IMPORTANTE
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideAnimations(), provideHttpClient(),
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimations(),
     provideAnimationsAsync(),
+    provideHttpClient(
+      withInterceptors([authInterceptor]) // <-- AQUI ADICIONA O INTERCEPTOR
+    ),
     providePrimeNG({
       theme: {
         preset: Aura
       }
-    })]
+    })
+  ]
 };
